706740a4e0a9c42af4154e3bfe710bbad1de4b1a
GuilhermePeres <guilherme.peresmn@gmail.com>
2024-11-30 17:32:11 -0300

    Ajustando o commit do projeto

---
+++ cadastro/src/main/java/br/com/restaurantes/cadastro/gateway/database/jpa/entity/RestauranteEntity.java
@@ -0,0 +1,23 @@
+package br.com.restaurantes.cadastro.gateway.database.jpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Data
+@Table(name="Restaurante")
+@Builder
+public class RestauranteEntity  {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private Integer quantidadeLugares;
+	private String localizacao;
+	private String tipoCozinha;
+	private String horarioFuncionamento;
+}
